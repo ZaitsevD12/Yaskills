5afa61797de46453400a5e636b40865bb544da57
SonyBvc <adg97487@gmail.com>
2023-03-19 00:49:35 +0300

    парсинг 1

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -7,8 +8,19 @@ app.listen(3000, (req, res) => {
     console.log("listening");
 })
 
+app.use(morgan('dev'));
+
+app.use((req, res) => {
+    console.log(req.body);
+});
+
 app.get('/', (req, res) => {
 
     res.sendFile('./src/index.html', { root: __dirname });
   
 });
+
+
+    
+
+
