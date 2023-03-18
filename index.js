c1706e362843e1702d2bc7509936b23c33dfa316
SonyBvc <adg97487@gmail.com>
2023-03-18 21:10:27 +0300

    мета тег для яндекс вебмасетра

---
+++ index.js
@@ -0,0 +1,9 @@
+<meta name="yandex-verification" content="88853bef697baa27" />
+
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, (req, res) => {
+    console.log("listening");
+})
